aaa312dd7a85746c0833f7c65332630f3f5ec19a
ahiromasa <akt.hrms@gmail.com>
2023-12-20 02:20:16 +0900

    1行あたりの文字数制限を緩和

--- src/main/scala/chapter03/Itinerary.scala
+++ src/main/scala/chapter03/Itinerary.scala
@@ -1,11 +1,7 @@
 package chapter03
 
 object Itinerary {
-  def replan(
-      plan: List[String],
-      newCity: String,
-      beforeCity: String
-  ): List[String] = {
+  def replan(plan: List[String], newCity: String, beforeCity: String): List[String] = {
     val beforeCityIndex = plan.indexOf(newCity)
     val citiesBefore = plan.slice(0, beforeCityIndex)
     val citiesAfter = plan.slice(beforeCityIndex, plan.size)
