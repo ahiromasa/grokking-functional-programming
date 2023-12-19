a17534ccb8b320dfbaaa7532bd0764ce177d7020
ahiromasa <akt.hrms@gmail.com>
2023-12-20 00:56:59 +0900

    3章を追加

---
+++ src/main/scala/chapter03/Name.scala
@@ -0,0 +1,10 @@
+package chapter03
+
+object Name {
+  def abbreviate(name: String): String = {
+    val initial = name.substring(0, 1)
+    val separator = name.indexOf(' ')
+    val lastName = name.substring(separator + 1)
+    initial + ". " + lastName
+  }
+}
